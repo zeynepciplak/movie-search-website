c413f6c351f905e0679841e1f4298da5cd19e6ca
Zeynep Cıplak <128297760+zeynepciplak@users.noreply.github.com>
2024-08-25 20:15:55 +0300

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+import './App.css';
+import { useTranslation } from 'react-i18next';
+
+
+
+
+const App:React.FC=()=> {
+  const {t} = useTranslation();//Bu kısımda i18n yi kullanmak için değişkenimizi tanımladım
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {t("Web sitemize hoş geldiniz!")}
+   
+  </div>
   );
-}
+} 
 
 export default App;
---
+++ src/i18n.ts
@@ -0,0 +1,33 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// LocalStorage'dan dil ayarını kontrol et
+const storedLang = localStorage.getItem('i18n');
+const defaultLang = 'en-US'; // Varsayılan dil
+
+// Eğer localStorage'da dil ayarı yoksa varsayılan dili ayarla
+if (!storedLang) {
+  localStorage.setItem('i18n', defaultLang);
+}
+
+// i18n yapılandırması
+i18n
+  .use(HttpApi) // çevirileri yüklemek için http backend kullanımı
+  .use(LanguageDetector) // kullanıcının dilini otomatik algılamak için
+  .use(initReactI18next) // react-i18next entegrasyonu
+  .init({
+    lng: localStorage.getItem('i18n') || defaultLang, // Dil ayarını al
+    supportedLngs: ['en', 'tr'], // Desteklenen diller
+    fallbackLng: 'en', // Bir dil bulunamazsa kullanılacak dil
+    debug: true, // Geliştirme sırasında hata ayıklama için
+    interpolation: {
+      escapeValue: false, // React zaten XSS koruması sağlar
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json', // Çeviri dosyalarının yolu
+    },
+  });
+
+export default i18n;
