2b69bf02ae3d003cf51c146429203e2eb513685b
Zeynep Cıplak <128297760+zeynepciplak@users.noreply.github.com>
2024-10-18 15:30:04 +0300

    hamburgermenu stil iyileştirmeleri

--- src/components/Icon/HamburgerMenu.tsx
+++ src/components/Icon/HamburgerMenu.tsx
@@ -1,188 +1,109 @@
 import React, { useState, useEffect } from 'react';
-import { Box, List, ListItem, ListItemText, Collapse, IconButton, AppBar, Toolbar, Menu, MenuItem, Typography } from '@mui/material';
+import { Box, List, ListItem, ListItemText, AppBar, Toolbar, Typography, Button, Drawer, Grid } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import '../../styles/global.css';
+import '../../components/Icon/HamburgerMenu.css';
 import { fetchMovieGenres, fetchTVGenres, Genre } from '../../api/tmdbApi';
 
 const HamburgerMenu: React.FC = () => {
-  const { t } = useTranslation();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [openGenre, setOpenGenre] = useState(false);  // Film ve dizi türleri için açılır/kapanır
-  const [openNewest, setOpenNewest] = useState(false);  // En yeni seçeneği için state
+  const { t, i18n } = useTranslation();
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const [movieGenres, setMovieGenres] = useState<Genre[]>([]);  // Film türleri
   const [tvGenres, setTVGenres] = useState<Genre[]>([]);  // Dizi türleri
   const navigate = useNavigate();
 
-  // Film ve dizi türlerini çek
+  // Film ve dizi türlerini çekiyoruz
   useEffect(() => {
     const loadGenres = async () => {
-      const movieGenresData = await fetchMovieGenres('en-US');  // Film türlerini çek
-      const tvGenresData = await fetchTVGenres('en-US');  // Dizi türlerini çek
+      const movieGenresData = await fetchMovieGenres(i18n.language);
+      const tvGenresData = await fetchTVGenres(i18n.language);
       setMovieGenres(movieGenresData);
       setTVGenres(tvGenresData);
     };
-
     loadGenres();
-  }, []);
+  }, [i18n.language]);
 
-  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
   };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  // Türler menüsünü aç/kapa
-  const handleGenreToggle = () => {
-    setOpenGenre(!openGenre);
-  };
-
-  // Tür tıklandığında yönlendirme
   const handleGenreClick = (genreId: number, mediaType: 'movie' | 'tv') => {
     if (mediaType === 'movie') {
-      navigate(`/movies/genre/${genreId}`);  // Filmlere yönlendirme
+      navigate(`/movies/genre/${genreId}`);
     } else {
-      navigate(`/series/genre/${genreId}`);  // Dizilere yönlendirme
+      navigate(`/series/genre/${genreId}`);
     }
-    handleMenuClose();  // Menü kapansın
-  };
-
-  const handleNewestClick = () => {
-    setOpenNewest(!openNewest);  // En yeni seçeneğini aç/kapa
-  };
-
-  const handleNewestMoviesClick = () => {
-    handleMenuClose();
-    navigate('/newest-movies');  // En yeni filmler sayfasına yönlendirme
-  };
-
-  const handleNewestSeriesClick = () => {
-    handleMenuClose();
-    navigate('/newest-series');  // En yeni diziler sayfasına yönlendirme
-  };
-
-  const handleIMDBTop100Click = () => {
-    handleMenuClose();
-    navigate('/imdb-top-100-movies');
-  };
-
-  const handleAwardWinningMoviesClick = () => {
-    handleMenuClose();
-    navigate('/awardwinningmovies');
-  };
-
-  const handleHomePageClick = () => {
-    handleMenuClose();
-    navigate('/');
-  };
-
-  const handleMostPopularArtistsClick = () => {
-    handleMenuClose();
-    navigate('/most-popular-artists');
-  };
-
-  const handleTopDirectorsClick = () => {
-    handleMenuClose();
-    navigate('/topdirectors');
-  };
-
-  const handleMoviesClick = () => {
-    handleMenuClose();
-    navigate('/movies');
-  };
-
-  const handleSeriesClick = () => {
-    handleMenuClose();
-    navigate('/series');
+    setDrawerOpen(false);  // Drawer'ı kapat
   };
 
   return (
-    <Box>
-      <AppBar className="appBar" position="static">
+    <Box className="hamburger-container">
+      <AppBar position="static" className="appBar">
         <Toolbar className="hamburger">
-          <IconButton color="inherit" onClick={handleMenuOpen}>
-            <MenuIcon className="hamburger" />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
+          <Button color="inherit" className="hamburger-button" onClick={toggleDrawer(true)}>
+            <MenuIcon />
+            <Typography variant="h6" sx={{ ml: 1 }}>{t('hamburgerMenu.Categories')}</Typography>
+          </Button>
         </Toolbar>
       </AppBar>
 
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleMenuClose}
-        sx={{ width: '100%' }}  // Responsive tam genişlik
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          style: {
+            width: '600px',
+            backgroundColor: '#FFD700',  // Sarı arka plan
+          },
+        }}
       >
-        <MenuItem onClick={handleHomePageClick}>
-          <ListItemText primary={t('hamburgerMenu.Home Page')} />
-        </MenuItem>
-
-        {/* Movies and Series Sections */}
-        <MenuItem onClick={handleMoviesClick}>
-          <ListItemText primary={t('hamburgerMenu.Movies')} />
-        </MenuItem>
-        <MenuItem onClick={handleSeriesClick}>
-          <ListItemText primary={t('hamburgerMenu.Series')} />
-        </MenuItem>
-
-        {/* Genre Sections */}
-        <MenuItem onClick={handleGenreToggle}>
-          <ListItemText primary={t('hamburgerMenu.Genres')} />
-          {openGenre ? <ExpandLess /> : <ExpandMore />}
-        </MenuItem>
-
-        <Collapse in={openGenre} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <Typography variant="subtitle1" sx={{ padding: '10px' }}>Movies</Typography>
-            {movieGenres.map((genre) => (
-              <ListItem button key={genre.id} sx={{ pl: 4 }} onClick={() => handleGenreClick(genre.id, 'movie')}>
-                <ListItemText primary={genre.name} />
-              </ListItem>
-            ))}
-            <Typography variant="subtitle1" sx={{ padding: '10px' }}>TV Series</Typography>
-            {tvGenres.map((genre) => (
-              <ListItem button key={genre.id} sx={{ pl: 4 }} onClick={() => handleGenreClick(genre.id, 'tv')}>
-                <ListItemText primary={genre.name} />
-              </ListItem>
-            ))}
-          </List>
-        </Collapse>
-
-        {/* The Newest Menu */}
-        <MenuItem onClick={handleNewestClick}>
-          <ListItemText primary={t('hamburgerMenu.The Newest')} />
-          {openNewest ? <ExpandLess /> : <ExpandMore />}
-        </MenuItem>
-
-        <Collapse in={openNewest} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button sx={{ pl: 4 }} onClick={handleNewestMoviesClick}>
-              <ListItemText primary={t('hamburgerMenu.Newest Movies')} />
+        <Box className="hamburger-drawer" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+          {/* Ana Sayfa ve diğer sabit başlıklar */}
+          <List>
+            <ListItem button onClick={() => navigate('/')}>
+              <ListItemText primary={t('hamburgerMenu.HomePage')} />
             </ListItem>
-            <ListItem button sx={{ pl: 4 }} onClick={handleNewestSeriesClick}>
-              <ListItemText primary={t('hamburgerMenu.Newest Series')} />
+            <ListItem button onClick={() => navigate('/top100movies')}>
+              <ListItemText primary={t('hamburgerMenu.Top100Movies')} />
+            </ListItem>
+            <ListItem button onClick={() => navigate('/awardwinningmovies')}>
+              <ListItemText primary={t('hamburgerMenu.AwardWinningMovies')} />
+            </ListItem>
+            <ListItem button onClick={() => navigate('/most-popular-artists')}>
+              <ListItemText primary={t('hamburgerMenu.MostPopularArtists')} />
+            </ListItem>
+            <ListItem button onClick={() => navigate('/topdirectors')}>
+              <ListItemText primary={t('hamburgerMenu.TopDirectors')} />
             </ListItem>
           </List>
-        </Collapse>
 
-        <MenuItem onClick={handleIMDBTop100Click}>
-          <ListItemText primary={t('hamburgerMenu.Top 100 Movies')} />
-        </MenuItem>
-        <MenuItem onClick={handleAwardWinningMoviesClick}>
-          <ListItemText primary={t('hamburgerMenu.Award Winning Movies')} />
-        </MenuItem>
-        <MenuItem onClick={handleMostPopularArtistsClick}>
-          <ListItemText primary={t('hamburgerMenu.Most Popular Artists')} />
-        </MenuItem>
-        <MenuItem onClick={handleTopDirectorsClick}>
-          <ListItemText primary={t('hamburgerMenu.Top Directors')} />
-        </MenuItem>
-      </Menu>
+          {/* Yatay genişleyen türler listesi */}
+          <Grid container spacing={2} className="menu-grid">
+            <Grid item xs={6}>
+              <Typography variant="subtitle1" className="category-header">{t('hamburgerMenu.Movies')}</Typography>
+              {movieGenres.map((genre) => (
+                <ListItem button key={genre.id} onClick={() => handleGenreClick(genre.id, 'movie')} className="menu-item">
+                  <ListItemText primary={genre.name} />
+                </ListItem>
+              ))}
+            </Grid>
+
+            <Grid item xs={6}>
+              <Typography variant="subtitle1" className="category-header">{t('hamburgerMenu.TVSeries')}</Typography>
+              {tvGenres.map((genre) => (
+                <ListItem button key={genre.id} onClick={() => handleGenreClick(genre.id, 'tv')} className="menu-item">
+                  <ListItemText primary={genre.name} />
+                </ListItem>
+              ))}
+            </Grid>
+          </Grid>
+        </Box>
+      </Drawer>
     </Box>
   );
 };
